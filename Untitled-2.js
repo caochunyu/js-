7635675f2c56bd645fa3808da6cf6a5665306caa
caochunyu <43447880+caochunyu@users.noreply.github.com>
2018-11-23 09:36:04 -0600

    Add files via upload

---
+++ Untitled-2.js
@@ -0,0 +1,9 @@
+//string对象的length属性，测量字符串长度
+var str1 = "this is a string";
+console.log(str1.length);
+
+var num1 = 1.45;
+console.log(typeof num1);
+
+var num1 = new Number(1.45）;
+console.log(typeof num1);
